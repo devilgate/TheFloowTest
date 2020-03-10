78b310f72a5bf49cf0076de17363aaa40e0d6a74
Martin McCallion <martin@devilgate.org>
2020-03-10 10:16:17 +0000

    Introduce MongoDB.

--- src/main/java/org/devilgate/floowtest/line/LineParseAndSave.java
+++ src/main/java/org/devilgate/floowtest/line/LineParseAndSave.java
@@ -4,30 +4,47 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @Service
 public class LineParseAndSave {
 
+	private MongoCollection<Document> wordStore;
+
 	private Map<String, Long> wordCounts = new HashMap<>();
 
-	public boolean processLine(String lineOfText) {
+	@Autowired
+	public LineParseAndSave(final MongoClient client) {
+		final MongoDatabase db = client.getDatabase("WordCount");
+		wordStore = db.getCollection("Words");
+	}
 
-		Line line = new Line(lineOfText);
+	public boolean processLine(String textLine) {
+
+		Line line = new Line(textLine);
 		return save(line.parse());
 	}
 
 	private boolean save(List<String> words) {
 
-		// For now, we just log and store in-memory.
 		for (String word : words) {
 			System.out.printf("Word: %s%n", word);
 
+
+			// wordStore.findOneAndUpdate()
 			long currentCount = 1;
-			if (wordCounts.containsKey(word)) {
-				currentCount = wordCounts.get(word) + 1;
-			}
-			wordCounts.put(word, currentCount);
+			wordStore.insertOne(new Document(word, currentCount));
+			// if (wordCounts.containsKey(word)) {
+			// 	currentCount = wordCounts.get(word) + 1;
+			// }
+			// wordCounts.put(word, currentCount);
 		}
 
 		// Send to the MongoDB server
---
+++ src/test/java/org/devilgate/floowtest/line/LineParseAndSaveShould.java
@@ -0,0 +1,58 @@
+package org.devilgate.floowtest.line;
+
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LineParseAndSaveShould {
+
+	private static final String LINE = "word again this that the other other word";
+
+	@Mock
+	private MongoClient client;
+
+	@Mock
+	private DB db;
+
+	@Mock
+	private DBCollection wordStore;
+
+	@Test
+	public void giveCorrectCountsOnSaving() {
+
+		when(client.getDB(anyString())).thenReturn(db);
+		when(db.getCollection(anyString())).thenReturn(wordStore);
+
+		var classUnderTest = new LineParseAndSave(client);
+		var notUsed = classUnderTest.processLine(LINE);
+		// var savedValues = classUnderTest.getWordCounts();
+		//
+		// assertEquals(1, savedValues.get("again"));
+		// assertEquals(2, savedValues.get("word"));
+		// assertEquals(2, savedValues.get("other"));
+		// assertEquals(1, savedValues.get("this"));
+		// assertEquals(1, savedValues.get("that"));
+		// assertEquals(1, savedValues.get("the"));
+	}
+
+	@Test
+	public void writeToMongo() {
+
+		MongoClient client = new MongoClient();
+		MongoDatabase db = client.getDatabase("WordCount");
+		MongoCollection<Document> collection = db.getCollection("Words");
+
+		var cut = new LineParseAndSave(client);
+		cut.processLine(LINE);
+	}
+}
